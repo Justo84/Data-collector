15ba07d3de94d0c8c88caa083846971a76de78cf
Justin Egan <justin.d.egan@gmail.com>
2022-12-09 09:42:43 -0500

    collected user data

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css"
 
 const AddUser = () => {
 
+    const [userName, setUserName] = useState('')
+    const [userAge, setUserAge] = useState('')
+
     const addUserHandler = (e) => {
         e.preventDefault()
-        console.log(e)
+
+        if (userName.trim().length === 0 || userAge.trim().length === 0) {
+            return
+        }
+
+        if (+userAge < 0) {
+            return
+        }
+
+        console.log(`name:${userName} age:${userAge}`)
+
+        setUserAge('')
+        setUserName('')
     }
 
-    const 
+    const addUserName = (e) => {
+        setUserName(e.target.value)
+    }
+    
+    const addUserAge = (e) => {
+        setUserAge(e.target.value)
+    }
 
     return (
         <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">User Name?</label>
-            <input id="username" type="text" />
+            <input id="username" type="text" value={userName} onChange={addUserName}/>
             <label htmlFor="age">Age?</label>
-            <input id="age" type="number" />
+            <input id="age" type="number" value={userAge} onChange={addUserAge} />
             <Button type="submit">Add Info</Button>
         </form>
         </Card>
